0951efddf3b87c2271442d661519c0043126fd56
Genki Takahashi <genki.takahashi.contact@gmail.com>
2017-08-14 18:05:46 +0100

    check User model and change a configulation on heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,6 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
+  # for members in lazy panda, we do not have to change anything in the next line.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
