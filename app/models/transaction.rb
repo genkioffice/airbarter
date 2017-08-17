1343737265556f4b7d1cf2b4d97b36d69a745d95
Genki Takahashi <genki.takahashi.contact@gmail.com>
2017-08-17 12:08:27 +0100

    made validation on transaction new file

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,6 +4,9 @@ class Transaction < ApplicationRecord
   belongs_to :wanted_product, :class_name => 'Product'
   belongs_to :accepted_by_user, :class_name => 'User', optional: true
 
+  validates :proposed_product_quantity, :numericality => {:greater_than => 1}
+  validates :wanted_product_quantity, :numericality => {:greater_than => 1}
+
   def change_status!(status)
     self.status = status
     self.save
