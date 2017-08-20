43395b87c4129d97846ed5da062029e67f9451c5
Finn Pedersen <fpeders2@orange.fr>
2017-08-20 09:07:45 +0100

    fixed business logic

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -5,9 +5,13 @@ class Entry < ApplicationRecord
 
   def self.remove_from_inventory(user, product, quantity)
     entry = self.where(user_id: user.id).where(product_id: product.id).first
-    if entry && entry.quantity > quantity
-      entry.quantity -= quantity
-      entry.save
+    if entry
+      if entry.quantity > quantity
+        entry.quantity -= quantity
+        entry.save
+      else
+        entry.destroy
+      end
     else
       # We should not arrive here, case should be prevented in the view.
     end
