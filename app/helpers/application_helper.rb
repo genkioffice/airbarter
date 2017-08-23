e49a1f6b0971ffadc493a8dac427b0d4e5fa1395
Finn Pedersen <fpeders2@orange.fr>
2017-08-23 21:45:04 +0100

    fixed bug in language choice when not logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
 
   def new_current_path(new_locale, current_path)
-    language_pattern = /\/(en|ja|da|ar|fr)\//
-    has_language = current_path[0..3] =~ language_pattern ? true : false
-    current_path = "/en" + current_path unless has_language
-    return "/" + new_locale + current_path[3..-1]
+    if current_path.length < 4
+      return "/" + new_locale
+    else
+      language_pattern = /\/(en|ja|da|ar|fr)\//
+      has_language = current_path[0..3] =~ language_pattern ? true : false
+      current_path = "/en" + current_path unless has_language
+      return "/" + new_locale + current_path[3..-1]
+    end
   end
 end
