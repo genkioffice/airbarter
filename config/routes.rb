0d7bef9b31f8aed2e9ccc6a8e15bb3c5207261d5
Finn Pedersen <fpeders2@orange.fr>
2017-08-23 14:23:21 +0100

    fixed some language issues

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,9 @@ Rails.application.routes.draw do
     get 'user',   to: 'pages#profile'
   end
 
-    devise_for :users, only: :omniauth_callbacks,
-      controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  # this is to handle facebook login without language
+  devise_for :users, only: :omniauth_callbacks,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
