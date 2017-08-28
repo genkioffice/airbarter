68917e860e5a86c93d5af9c80b463ff943821d5a
Genki Takahashi <genki.takahashi.contact@gmail.com>
2017-08-28 16:28:14 +0100

    change routes and device controller to update correctly

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,73 +1,227 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  prepend_before_action :require_no_authentication, only: [:new, :create, :cancel]
+  prepend_before_action :authenticate_scope!, only: [:edit, :update, :destroy]
+  prepend_before_action :set_minimum_password_length, only: [:new, :edit]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    build_resource({})
+    yield resource if block_given?
+    respond_with resource
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
 
   # GET /resource/edit
   def edit
-    super
-    byebug
-    @user = current_user
+    render :edit
   end
 
   # PUT /resource
+  # We need to use a copy of the resource because we don't want to change
+  # the current user in place.
   def update
-    super
-    @user = current_user
-byebug
-    if @user = User.update(user_params)
-      redirect_to edit_user_registeration_path
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+    # on the line below, used user params instead of account update params because of the simple form for user
+    resource_updated = update_resource(resource, user_params)
+    yield resource if block_given?
+    if resource_updated
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      bypass_sign_in resource, scope: resource_name
+      respond_with resource, location: after_update_path_for(resource)
     else
-      render :edit
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
     end
   end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+    resource.destroy
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message! :notice, :destroyed
+    yield resource if block_given?
+    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
   # cancel oauth signing in/up in the middle of the process,
   # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  def cancel
+    expire_data_after_sign_in!
+    redirect_to new_registration_path(resource_name)
+  end
 
   protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def update_needs_confirmation?(resource, previous)
+    resource.respond_to?(:pending_reconfirmation?) &&
+      resource.pending_reconfirmation? &&
+      previous != resource.unconfirmed_email
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :address, :facebook_picture_url, :first_name, :last_name, :photo])
+  # By default we want to require a password checks on update.
+  # You can overwrite this method in your own RegistrationsController.
+  def update_resource(resource, params)
+    resource.update_with_password(params)
+  end
 
+  # Build a devise resource passing in the session. Useful to move
+  # temporary session data to the newly created user.
+  def build_resource(hash=nil)
+    self.resource = resource_class.new_with_session(hash || {}, session)
+  end
+
+  # Signs in a user on sign up. You can overwrite this method in your own
+  # RegistrationsController.
+  def sign_up(resource_name, resource)
+    sign_in(resource_name, resource)
+  end
+
+  # The path used after sign up. You need to overwrite this method
+  # in your own RegistrationsController.
+  def after_sign_up_path_for(resource)
+    after_sign_in_path_for(resource)
+  end
+
+  # The path used after sign up for inactive accounts. You need to overwrite
+  # this method in your own RegistrationsController.
+  def after_inactive_sign_up_path_for(resource)
+    scope = Devise::Mapping.find_scope!(resource)
+    router_name = Devise.mappings[scope].router_name
+    context = router_name ? send(router_name) : self
+    context.respond_to?(:root_path) ? context.root_path : "/"
+  end
+
+  # The default url to be used after updating a resource. You need to overwrite
+  # this method in your own RegistrationsController.
+  def after_update_path_for(resource)
+    '/users/edit'
+  end
+
+  # Authenticates the current scope and gets the current resource from the session.
+  def authenticate_scope!
+    send(:"authenticate_#{resource_name}!", force: true)
+    self.resource = send(:"current_#{resource_name}")
+  end
+
+
+  def sign_up_params
+    devise_parameter_sanitizer.sanitize(:sign_up)
+  end
+
+  def account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :address, :facebook_picture_url, :first_name, :last_name, :photo, :password, :password_confirmation, :current_password])
   end
 
   def user_params
-  params.require(:user).permit(:email, :address, :facebook_picture_url, :first_name, :last_name, :photo)
+    params.require(:user).permit(:email, :address, :facebook_picture_url, :first_name, :last_name, :photo, :password, :password_confirmation, :current_password)
   end
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def translation_scope
+    'devise.registrations'
+  end
+
+  def after_sign_up_path_for(resource)
+    show_users_path(resource)
+  end
 end
+
+# class Users::RegistrationsController < Devise::RegistrationsController
+#   # before_action :configure_sign_up_params, only: [:create]
+#   before_action :configure_account_update_params, only: [:update]
+
+#   # GET /resource/sign_up
+#   # def new
+#   #   super
+#   # end
+
+#   # POST /resource
+#   # def create
+#   #   super
+#   # end
+
+#   # GET /resource/edit
+#   def edit
+#     super
+#     @user = current_user
+#   end
+
+#   # PUT /resource
+#   def update
+#     @user = current_user
+#     if @user = User.update(user_params)
+#       redirect_to edit_user_registration_path
+#     else
+#       render :edit
+#     end
+#   end
+
+#   # DELETE /resource
+#   # def destroy
+#   #   super
+#   # end
+
+#   # GET /resource/cancel
+#   # Forces the session data which is usually expired after sign
+#   # in to be expired now. This is useful if the user wants to
+#   # cancel oauth signing in/up in the middle of the process,
+#   # removing all OAuth session data.
+#   # def cancel
+#   #   super
+#   # end
+
+#   protected
+
+#   # If you have extra params to permit, append them to the sanitizer.
+#   # def configure_sign_up_params
+#   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+#   # end
+
+#   # If you have extra params to permit, append them to the sanitizer.
+#   def configure_account_update_params
+#     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :address, :facebook_picture_url, :first_name, :last_name, :photo, :password, :password_confirmation, :current_password])
+#   end
+
+#   def user_params
+#   params.require(:user).permit(:email, :address, :facebook_picture_url, :first_name, :last_name, :photo, :password)
+#   end
+#   # The path used after sign up.
+#   # def after_sign_up_path_for(resource)
+#   #   super(resource)
+#   # end
+
+#   # The path used after sign up for inactive accounts.
+#   # def after_inactive_sign_up_path_for(resource)
+#   #   super(resource)
+#   # end
+# end
