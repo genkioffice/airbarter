ef45d13d6415d758e65f77dcacce687b0ce44e16
Finn Pedersen <fpeders2@orange.fr>
2017-08-31 14:57:42 +0100

    adding container class to views and added picture on product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -73,6 +73,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :name_ja, :name_da, :name_ar, :name_fr)
+      params.require(:product).permit(:name, :name_ja, :name_da, :name_ar, :name_fr, :photo)
     end
 end
